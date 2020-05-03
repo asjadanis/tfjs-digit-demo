4e68e067da37e591d8a8e02771b9cf6e590e8285
asjadanis <asjadanis128@gmail.com>
2020-05-03 15:29:44 +0500

    removed browser router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import routes from "./routes/routes.json";
 import "./App.css";
