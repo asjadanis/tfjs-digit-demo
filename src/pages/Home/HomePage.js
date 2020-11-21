04253aeffd41b6a6e5eaddc6e6f639418e6dd288
asjadanis <asjadanis128@gmail.com>
2020-11-21 17:57:44 +0500

    responsive changes

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -7,9 +7,9 @@ const HomePage = (props) => {
 
   return (
     <div className="home-container">
-      <div style={{display: "flex", height: "100%", alignItems:"center"}}>
-        <div style={{width:"50%"}}><DigitCanvas /></div>
-        <div style={{width:"50%"}}><DigitCard digit={5}/> </div>
+      <div className="inner-container">
+        <div style={{width:"50%", minWidth: "300px"}}><DigitCanvas /></div>
+        <div style={{width:"50%", minWidth: "300px"}}><DigitCard digit={5}/> </div>
       </div>
     </div>
   );
