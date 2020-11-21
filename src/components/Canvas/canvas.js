d2eca5e40d09ccdbf9d201f21b582a809161c6ba
asjadanis <asjadanis128@gmail.com>
2020-11-21 18:41:38 +0500

    formatted code

--- src/components/Canvas/canvas.js
+++ src/components/Canvas/canvas.js
@@ -1,12 +1,12 @@
-import React  from "react";
-import "./canvas.css"
+import React from "react";
+import "./canvas.css";
 
 const DigitCanvas = (props) => {
   return (
     <div>
       <canvas width={"250px"} height={"250px"} className="canvas-styles"></canvas>
     </div>
-  )
-}
+  );
+};
 
-export default DigitCanvas
+export default DigitCanvas;
