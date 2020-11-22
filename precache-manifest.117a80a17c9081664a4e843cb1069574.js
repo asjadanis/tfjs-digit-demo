012f4402af5a8f5e48a7961d59d6b116beeec318
asjadanis <asjadanis128@gmail.com>
2020-11-22 17:17:27 +0500

    Updates

---
+++ precache-manifest.117a80a17c9081664a4e843cb1069574.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e9759d59bb418144711ee82c45cb4c",
+    "url": "/tfjs-digit-demo/index.html"
+  },
+  {
+    "revision": "f3a41d2db386b8e4064e",
+    "url": "/tfjs-digit-demo/static/css/2.48a77e21.chunk.css"
+  },
+  {
+    "revision": "91f31ec5a151fa8bf18f",
+    "url": "/tfjs-digit-demo/static/css/main.7c4e559a.chunk.css"
+  },
+  {
+    "revision": "f3a41d2db386b8e4064e",
+    "url": "/tfjs-digit-demo/static/js/2.1754eade.chunk.js"
+  },
+  {
+    "revision": "7ce9b01320880f9560196a3592238833",
+    "url": "/tfjs-digit-demo/static/js/2.1754eade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91f31ec5a151fa8bf18f",
+    "url": "/tfjs-digit-demo/static/js/main.5b11208c.chunk.js"
+  },
+  {
+    "revision": "bfd55d8850d74181ac1f",
+    "url": "/tfjs-digit-demo/static/js/runtime-main.47459390.js"
+  }
+]);
