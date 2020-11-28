ca13c2a26f0703f373cc0a6a2422c5ca0cf7e007
asjadanis <asjadanis128@gmail.com>
2020-11-28 14:09:36 +0500

    added comment

--- src/modules/predict.js
+++ src/modules/predict.js
@@ -3,7 +3,7 @@ import * as tf from "@tensorflow/tfjs";
 export const processCanvasData = (dataString) => {
   const image = tf.browser.fromPixels(dataString);
   const resizedImage = image.resizeNearestNeighbor([28, 28]);
-  const mean = resizedImage.mean(2);
+  const mean = resizedImage.mean(2); // reduce rank
   const reshapedImage = mean.expandDims(2);
   const expandedDimensions = reshapedImage.expandDims().toFloat();
   const average = expandedDimensions.div(255.0);
